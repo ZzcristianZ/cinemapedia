6415663dadccb8d09e1729dd534fa4707e3082c0
cristian <cdarenizc@ufpso.edu.co>
2026-07-15 18:02:10 -0500

    prueba

--- lib/presentation/screens/movies/movie_screen.dart
+++ lib/presentation/screens/movies/movie_screen.dart
@@ -54,7 +54,6 @@ class _MovieDatils extends StatelessWidget {
   const _MovieDatils({required this.movie});
   @override
   Widget build(BuildContext context) {
-    final colors = Theme.of(context).colorScheme;
     final texStyles = Theme.of(context).textTheme;
     final size = MediaQuery.of(context).size;
 
@@ -97,10 +96,6 @@ class _MovieDatils extends StatelessWidget {
             ))
           ],),
       ),
-
-
-
-
         const SizedBox(height: 100,)
       ],
     );
